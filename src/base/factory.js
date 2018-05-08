bcca1ba545328cd0051bd2c773722af48eca476c
ganyangxing <172176234@qq.com>
2018-05-09 00:43:41 +0800

    init commit

--- src/base/factory.js
+++ src/base/factory.js
@@ -7,6 +7,21 @@ const Factory = {
         let sql = `select * from ${table} where id=${id}`;
         let data = await Sqlext.exec(sql);
         return data[0];
+    },
+    query: async (table, params) => {
+
+    },
+    add: async (table, params) => {
+
+    },
+    update: async (table, id, params) => {
+
+    },
+    remove: async (table, id) => {
+
+    },
+    exec: async (sql, values) => {
+        return await Sqlext.exec(sql, values);
     }
 };
 
